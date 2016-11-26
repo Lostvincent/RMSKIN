6470357d1f1d4062d14183d5f679198d9e65a2ba
Vincent <lostvincent7@gmail.com>
2016-11-26 17:21:59 +0800

    delete old key and move to other place

--- config/filesystems.php
+++ config/filesystems.php
@@ -69,8 +69,8 @@ return [
                 'https'     => 'oh74y7qhm.qnssl.com',   //你的HTTPS域名
                 'custom'    => 'www.rmskin.net',                //你的自定义域名
              ],
-            'access_key'=> 'PvnqQmmG5bhvGucXATJUOCnxf-aaQIZbKLJqHqWn',  //AccessKey
-            'secret_key'=> 'QBX8ZZ3TR7yxMqvblFG8WZbmbl0vwtgKvtkB55WX',  //SecretKey
+            'access_key'=> env('access_key'),  //AccessKey
+            'secret_key'=> env('secret_key'),  //SecretKey
             'bucket'    => 'rmskin',  //Bucket名字
             'notify_url'=> '',  //持久化处理回调地址
         ],
